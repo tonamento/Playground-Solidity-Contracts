7bea405688993f6807926ce6d7778b96c1b59449
root <root@sehele16732.ptr1.ru>
2023-12-02 09:04:38 +0200

    fixing sending funcs

--- contracts/TicketMaster.sol
+++ contracts/TicketMaster.sol
@@ -5,47 +5,56 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TicketMaster {
     address public owner;
-    IERC20 usdc = IERC20(0x43B341FBAE05D3Bfa351362d11783347E184050d);
+    IERC20 public usdc;
 
-    uint128 ticketBuyRate = 17.23 ether;
-    uint128 ticketSellRate = 17.454 ether;
+    uint256 public ticketBuyRate; // 1 USDC = ticketBuyRate TOTO
+    uint256 public ticketSellRate; // 1 USDC = ticketSellRate TOTO
     
-    mapping(address => uint) public tickets;
+    mapping(address => uint256) public tickets;
 
-    event TicketPurchased(address indexed buyer, uint amount);
-    event TicketUsed(address indexed user, uint amount);
+    event TicketPurchased(address indexed buyer, uint256 amount);
+    event TicketSold(address indexed seller, uint256 amount);
+    event TicketUsed(address indexed user, uint256 amount);
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not the contract owner");
+        require(msg.sender == owner, "Only the contract owner can call this function");
         _;
     }
 
-    constructor() {
+    constructor(address _usdcAddress, uint256 _buyRate, uint256 _sellRate) {
         owner = msg.sender;
+        usdc = IERC20(_usdcAddress);
+        ticketBuyRate = _buyRate;
+        ticketSellRate = _sellRate;
     }
 
-    function buyTicket(uint128 _amount) external returns (bool) {
-        require(usdc.balanceOf(msg.sender) >= _amount, 'Insufficient balance');
-        require(usdc.allowance(msg.sender, address(this)) >= _amount, 'Insufficient allowance');
+    function buyTicket(uint256 _usdcAmount) external returns (bool) {
+        require(usdc.balanceOf(msg.sender) >= _usdcAmount, 'Insufficient USDC balance');
+        require(usdc.allowance(msg.sender, address(this)) >= _usdcAmount, 'Insufficient allowance');
 
-        uint256 costInTokens = _amount / ticketBuyRate;
-        require(usdc.transferFrom(msg.sender, address(this), _amount));
-        
-        tickets[msg.sender] = costInTokens;
+        uint256 costInTokens = _usdcAmount * ticketBuyRate;
+        require(usdc.transferFrom(msg.sender, address(this), _usdcAmount), "Transfer of USDC failed");
+
+        tickets[msg.sender] += costInTokens; 
+        emit TicketPurchased(msg.sender, costInTokens);
         return true;
     }
 
-    function sellingTicket(uint128 _amount) external returns (bool) {
-        require(tickets[msg.sender] >= _amount , 'Insufficient ticket balance');
-        require(usdc.transfer(msg.sender, _amount / ticketSellRate));
+    function sellTicket(uint256 _tokensAmount) external returns (bool) {
+        require(tickets[msg.sender] >= _tokensAmount, 'Insufficient ticket balance');
+
+        uint256 amountInUSDC = _tokensAmount / ticketSellRate;
+        require(usdc.transfer(msg.sender, amountInUSDC), "Transfer of USDC failed");
         
-        tickets[msg.sender] -= _amount * ticketSellRate;
+        tickets[msg.sender] -= _tokensAmount;
+        emit TicketSold(msg.sender, amountInUSDC);
         return true;
     }
 
-     function useTicket(uint128 _amount) external returns (bool) {
-        require(usdc.balanceOf(msg.sender) > _amount , 'Insufficient balance');
-        tickets[msg.sender] -= _amount;
+    function useTicket(uint256 _tokensAmount) external returns (bool) {
+        require(tickets[msg.sender] >= _tokensAmount, 'Insufficient ticket balance');
+        tickets[msg.sender] -= _tokensAmount;
+        emit TicketUsed(msg.sender, _tokensAmount);
         return true;
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,12 @@
 const hre = require("hardhat");
+const web3 = require("web3");
 
 async function main() {
 
-  const reportContract = await hre.ethers.deployContract("TicketMaster");
+  const buyRate = web3.utils.toWei("17.23");
+  const sellRate = web3.utils.toWei("17.454");
 
+  const reportContract = await hre.ethers.deployContract("TicketMaster", ["0x43B341FBAE05D3Bfa351362d11783347E184050d", buyRate, sellRate]);
   await reportContract.waitForDeployment();
 
   console.log(
